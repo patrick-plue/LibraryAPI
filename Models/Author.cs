0e479b98f26eba9d03301a295d39216a5a329cbb
Patrick Pluequett <patrick.pluequett@wbscodingschool.com>
2026-06-29 10:47:12 +0200

    add author model

---
+++ Models/Author.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.SignalR;
+
+public class Author
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+
+    public string? FirstName { get; set; } = string.Empty;
+
+    public required string LastName { get; set; }
+
+    public string? Biography
+    {
+        get; set;
+    }
+
+    public DateOnly BirthDate { get; set; }
+
+    public DateOnly? DeathDate { get; set; }
+}
