e6c632e0f2cf4b447a22f1ca468934055ecb2721
Patrick Pluequett <patrick.pluequett@wbscodingschool.com>
2026-06-29 10:44:10 +0200

    add book model

---
+++ Models/Book.cs
@@ -0,0 +1,21 @@
+public class Book
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+
+    public required string Title { get; set; } = string.Empty;
+
+    public required Guid AuthorId { get; set; }
+
+    public string? ISBN { get; set; }
+
+    public string? Description { get; set; }
+
+    public DateOnly? PublishedDate { get; set; }
+
+    public string? Genre { get; set; }
+
+    public int PageCount { get; set; }
+
+    public string? Language { get; set; }
+
+}
