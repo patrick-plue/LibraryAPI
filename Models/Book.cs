aa8166261e892b77f38b1272a4bb28a86bb5354c
Patrick Pluequett <patrick.pluequett@wbscodingschool.com>
2026-06-30 11:10:11 +0200

    change book class to book record

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,23 +1,13 @@
 namespace LibraryAPI.Models;
 
-public class Book
+public record Book
 {
-    public Guid Id { get; init; } = Guid.NewGuid();
-
-    public required string Title { get; set; } = string.Empty;
-
-    public required Guid AuthorId { get; set; }
-
-    public string? ISBN { get; set; }
-
+    public Guid Id { get; init; }
+    public required string Title { get; set; }
+    public Guid AuthorId { get; set; }
+    public string? Isbn { get; set; }
     public string? Description { get; set; }
-
     public DateOnly? PublishedDate { get; set; }
-
     public string? Genre { get; set; }
-
-    public int PageCount { get; set; }
-
-    public string? Language { get; set; }
-
+    public int? PageCount { get; set; }
 }
