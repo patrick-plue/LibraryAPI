16ec434a623e4f718f9595dbcd124c801b40a483
Patrick Pluequett <patrick.pluequett@wbscodingschool.com>
2026-07-03 14:53:45 +0200

    add interface author service

---
+++ Application/Interface/IAuthorService.cs
@@ -0,0 +1,17 @@
+using LibraryAPI.Models;
+
+namespace LibraryAPI.Application.Interface;
+
+public interface IAuthorService
+{
+    public Author AddAuthor(string? firstName, string lastName, string? biography, DateOnly? BirthDate, DateOnly? DeathDate);
+
+    public bool RemoveAuthor(Guid id);
+
+    public IEnumerable<Author>? ListAuthors();
+
+    public Author? GetAuthorById(Guid id);
+
+    public Author? UpdateAuthor(Guid id, string? firstName, string? lastName, string? biography, DateOnly? BirthDate, DateOnly? DeathDate);
+
+}
